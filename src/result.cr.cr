bfd6ad8bd4b6c7b540f476b81bf064f3b8ab5e4b
Andy Nicholson <andrew@anicholson.net>
2018-06-06 12:01:51 +1000

    crystal init lib result.cr

---
+++ src/result.cr.cr
@@ -0,0 +1,6 @@
+require "./result.cr/*"
+
+# TODO: Write documentation for `Result.cr`
+module Result.cr
+  # TODO: Put your code here
+end
---
+++ src/result.cr/version.cr
@@ -0,0 +1,3 @@
+module Result.cr
+  VERSION = "0.1.0"
+end
