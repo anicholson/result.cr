0ed3e677b4b0177b6ac3463d5f66f6d197123757
Andy Nicholson <andrew@anicholson.net>
2020-07-13 11:27:51 +1000

    Fix pipeline generator

--- .buildkite/pipeline.sh
+++ .buildkite/pipeline.sh
@@ -13,20 +13,20 @@ for v in ${versions[@]}; do
   echo " - label: Run Specs (${v})"
   echo "   command: crystal spec"
   echo "   plugins:"
-  echo '     - "docker-compose#v3.2.0":'
-  echo "       run: 'ci-${v}'"
+  echo '     - docker-compose#v3.2.0:'
+  echo "         run: ci-${v}"
 
   # Formatting
   echo " - label: Code Formatting (${v})"
   echo "   command: bin/ameba"
   echo "   plugins:"
-  echo '     - "docker-compose#v3.2.0":'
-  echo "       run: 'ci-${v}'"
+  echo '     - docker-compose#v3.2.0:'
+  echo "         run: ci-${v}"
 
   # Mutation testing
   echo " - label: Mutation Testing (${v})"
   echo "   command: bin/crytic -m 68"
   echo "   plugins:"
-  echo '     - "docker-compose#v3.2.0":'
-  echo "       run: 'ci-${v}'"
+  echo '     - docker-compose#v3.2.0:'
+  echo "         run: ci-${v}"
 done
