08d1d31505ebc98efb61cc880ac805c2ab98b37a
Andy Nicholson <andrew@anicholson.net>
2020-07-13 10:39:37 +1000

    Use Buildkite for CI

---
+++ .buildkite/pipeline.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+set -e
+set -u
+set -o errexit
+
+declare -a versions=('0.35.0' '0.34.0' '0.33.0')
+
+echo "steps:"
+
+for v in ${versions[@]}; do
+  # Specs
+  echo " - label: Run Specs (${v})"
+  echo "   command: crystal spec"
+  echo "   plugins:"
+  echo '     - "docker-compose#v3.2.0":'
+  echo "       run: 'ci-${v}'"
+
+  # Formatting
+  echo " - label: Code Formatting (${v})"
+  echo "   command: bin/ameba"
+  echo "   plugins:"
+  echo '     - "docker-compose#v3.2.0":'
+  echo "       run: 'ci-${v}'"
+
+  # Mutation testing
+  echo " - label: Mutation Testing (${v})"
+  echo "   command: bin/crytic -m 68"
+  echo "   plugins:"
+  echo '     - "docker-compose#v3.2.0":'
+  echo "       run: 'ci-${v}'"
+done
